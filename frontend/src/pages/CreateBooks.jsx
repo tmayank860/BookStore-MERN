5ac977a6afc27d0c2ab5c1766057fa159931ac17
tmayank860 <tmayank860@gmail.com>
2023-12-23 00:00:48 +0530

    fix crash issue

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 
